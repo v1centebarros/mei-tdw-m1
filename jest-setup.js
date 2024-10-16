3b285efbcefdb38269c72ae517bcf8f07b222c5d
v1centebarros <vmabarros@ua.pt>
2024-10-16 10:28:43 +0100

    first test

---
+++ jest-setup.js
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom'
+
+export default {
+	transform: {
+		'^.+\\.svelte$': 'svelte-jester',
+	},
+	moduleFileExtensions: ['js', 'svelte'],
+	extensionsToTreatAsEsm: ['.svelte'],
+	testEnvironment: 'jsdom',
+	setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
+}
