55f43372793152c5efae5062051bf39dfe2dec61
v1centebarros <vmabarros@ua.pt>
2024-10-19 15:21:10 +0100

    upgrade ci/cd

--- src/routes/(todos)/+layout.svelte
+++ src/routes/(todos)/+layout.svelte
@@ -1,3 +1,4 @@
 <div class="card mx-auto w-1/2 bg-white">
 	<slot></slot>
 </div>
+<p class="text-3xl text-white">Find some awesome news <a data-sveltekit-preload-data="hover" class="text-orange-400 hover:underline " href="/news">here</a>!</p>
